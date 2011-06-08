33c10e99c3c08cd5f56c1169a1fc50159d876fbf
Tom Philpot <tom.philpot@logos.com>
2011-06-07 22:21:34 -0700

    Minor changes.

--- doStuff.sh
+++ doStuff.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
-incrementFile()
-{
+incrementFile() {
 	MY_NUMBER=`cat theFile.txt`
 	((THE_NUM=MY_NUMBER + 2))
-	echo $THE_NUM > theFile.txt
+	echo $THE_NUM >> theFile.txt
 }
 
-#commitFile()
-# {
-#	git add theFile.txt
-#	git commit -m "Updating the file."
-#}
+
+addAndCommitFile() {
+	git add theFile.txt
+	git commit -m "Updating the file."
+}
 
 # Create the file if it doesn't exist
+createFile() {
 if [[ ! -f theFile.txt ]]
 then
 	echo 0 > theFile.txt;
 fi
+}
+
+createFile
 
 incrementFile
 
-#commitFile
+#addAndCommitFile

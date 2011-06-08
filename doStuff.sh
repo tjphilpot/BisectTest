b8c23a7a4d9244fe921ee6301c9be77467ec5213
Tom Philpot <tom.philpot@logos.com>
2011-06-07 22:44:42 -0700

    Allow use of arbitrary file name

--- doStuff.sh
+++ doStuff.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-set -x
+# Use the first parameter passed the script, otherwise, use theFile.txt as the filename.
+MY_FILE=${1:-theFile.txt}
 
 incrementFile() {
-	MY_NUMBER=`tail -n 1 theFile.txt`
+	MY_NUMBER=`tail -n 1 $MY_FILE`
 	((THE_NUM=MY_NUMBER + 2))
-	echo $THE_NUM >> theFile.txt
+	echo $THE_NUM >> $MY_FILE
 }
 
 
 # Create the file if it doesn't exist
 createFile() {
-if [[ ! -f theFile.txt ]]
+if [[ ! -f $MY_FILE ]]
 then
-	echo 0 > theFile.txt;
+	echo 0 > $MY_FILE;
 fi
 }
 

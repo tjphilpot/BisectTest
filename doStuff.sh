cdeab9f49fd7ed79d82cff3c9d1ef33b606593f2
Tom Philpot <tom.philpot@logos.com>
2011-06-07 17:07:28 -0700

    Initial commit

---
+++ doStuff.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+incrementFile()
+{
+	MY_NUMBER=`cat theFile.txt`
+	echo $(($MY_NUMBER + 2)) >> theFile.txt
+}
+
+#commitFile()
+# {
+#	git add theFile.txt
+#	git commit -m "Updating the file."
+#}
+
+# Create the file if it doesn't exist
+#if [[ ! -f theFile.txt ]]
+#then
+#	echo 0 > theFile.txt;
+#if
+
+#incrementFile
+
+#commitFile

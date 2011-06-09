149079e25780318e5fc9e9076edebf56d92641c2
Tom Philpot <tom.philpot@logos.com>
2011-06-08 21:37:37 -0700

    Adding the date and time to the build generated file.

--- incrementBuildNumber.sh
+++ incrementBuildNumber.sh
@@ -16,4 +16,6 @@ createFile;
 # Read in a build number from a file, update it and write it back out.
 BUILD_NUMBER=`tail -n 1 $BUILD_NUMBER_FILE | awk '{print $1}'`
 ((NEW_NUMBER=BUILD_NUMBER + 1))
-echo $NEW_NUMBER >> $BUILD_NUMBER_FILE
+DATE=`date`
+echo "Generating build $NEW_NUMBER at $DATE"
+echo $NEW_NUMBER $DATE >> $BUILD_NUMBER_FILE
